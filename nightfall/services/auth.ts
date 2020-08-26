4a1e104b2425253254558853112dd22f8853a6e3
Antoine GUENEAU <gueneau.antoine@gmail.com>
2020-08-26 13:33:26 +0200

    user call api

--- nightfall/services/auth.ts
+++ nightfall/services/auth.ts
@@ -5,20 +5,5 @@ import fetchRequest from "../models/FetchRequest";
  * Get authenticate user
  */
 export const getAuthUser = async (): Promise<User> => {
-
-    let result = await fetchRequest({path: "/establishments", method: "GET"})
-    
-    console.log("result");
-    console.log(result);
-
-    return {
-        id: 1,
-        login: "AG49",
-        firstname: "Antoine",
-        lastname: "GUENEAU",
-        avatar:
-            "https://i2.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?resize=256%2C256&quality=100&ssl=1",
-        countPointsLastMonth: 460,
-        countPointsLastYear: 685,
-    } as User;
+    return (await fetchRequest({path: "/users/5f44fe6f575b685ae5ce83ee", method: "GET"})) as User
 };
