514b3eda281a999741f5edc0166c19f8947ceb24
Dimitri SAUVAGE <dimitri1993@live.fr>
2020-08-27 09:59:20 +0200

    Update establishment model

--- api/src/database/fakes/entities/establishments.ts
+++ api/src/database/fakes/entities/establishments.ts
@@ -2,46 +2,93 @@ import { EstablishmentModel } from "../../../models/Establishment";
 import { TypeEstablishmentModel } from "../../../models/Type_establishment";
 
 const fakeEstablishments = async function () {
-    const type_bar = await TypeEstablishmentModel.findOne({ name: "Bar" }).exec();
+    const type_bar = await TypeEstablishmentModel.findOne({
+        name: "Bar",
+    }).exec();
 
-    const type_restos = await TypeEstablishmentModel.findOne({
+    const type_resto = await TypeEstablishmentModel.findOne({
         name: "Restaurant",
     }).exec();
 
-    const type_discotheques = await TypeEstablishmentModel.findOne({
+    const type_discotheque = await TypeEstablishmentModel.findOne({
         name: "Discothèque",
     }).exec();
 
     const establishments = [
         new EstablishmentModel({
-            establishment_type_id: type_bar?._id,
+            establishment_type_id: type_bar?._id.toString(),
             name: "La Civette",
-            latitude: 47.473204,
-            longitude: -0.551635,
+            address: {
+                number: "2",
+                street: "pl Romain",
+                zipCode: "49100",
+                city: "Angers",
+                coordinate: {
+                    latitude: 47.473204,
+                    longitude: -0.551635,
+                },
+            },
+            establishment_type: type_bar?._id.toString(),
         }),
         new EstablishmentModel({
-            establishment_type_id: type_bar?._id,
+            establishment_type_id: type_bar?._id.toString(),
             name: "Le James Joyce",
-            latitude: 47.474032,
-            longitude: -0.545825,
+            address: {
+                number: "40",
+                street: "Boulevard Carnot",
+                zipCode: "49100",
+                city: "Angers",
+                coordinate: {
+                    latitude: 47.474032,
+                    longitude: -0.545825,
+                },
+            },
+            establishment_type: type_bar?._id.toString(),
         }),
         new EstablishmentModel({
-            establishment_type_id: type_restos?._id,
+            establishment_type_id: type_resto?._id.toString(),
             name: "Chez Pont-pont",
-            latitude: 47.470281,
-            longitude: -0.558083,
+            address: {
+                number: "13",
+                street: "Promenade du Bout du Monde",
+                zipCode: "49100",
+                city: "Angers",
+                coordinate: {
+                    latitude: 47.470281,
+                    longitude: -0.558083,
+                },
+            },
+            establishment_type: type_resto?._id.toString(),
         }),
         new EstablishmentModel({
-            establishment_type_id: type_restos?._id,
+            establishment_type_id: type_resto?._id.toString(),
             name: "La Ferme",
-            latitude: 47.470666,
-            longitude: -0.555754,
+            address: {
+                number: "2",
+                street: "Place Freppel",
+                zipCode: "49100",
+                city: "Angers",
+                coordinate: {
+                    latitude: 47.470666,
+                    longitude: -0.555754,
+                },
+            },
+            establishment_type: type_resto?._id.toString(),
         }),
         new EstablishmentModel({
-            establishment_type_id: type_discotheques?._id,
+            establishment_type_id: type_discotheque?._id.toString(),
             name: "La Carré",
-            latitude: 47.473243,
-            longitude: -0.553601,
+            address: {
+                number: "15",
+                street: "Rue de la Roë",
+                zipCode: "49100",
+                city: "Angers",
+                coordinate: {
+                    latitude: 47.473243,
+                    longitude: -0.553601,
+                },
+            },
+            establishment_type: type_discotheque?._id.toString(),
         }),
     ];
 
--- api/src/models/Establishment.ts
+++ api/src/models/Establishment.ts
@@ -1,42 +1,65 @@
-import { model, Schema, Model, Document } from "mongoose";
+import { Document, model, Model, Schema } from "mongoose";
 import { ITypeEstablishment } from "./Type_establishment";
-
 const EstablishmentSchema: Schema = new Schema({
     name: {
         type: String,
         require: true,
     },
     establishment_type_id: {
-        type: Schema.Types.ObjectId,
+        type: String,
         required: true,
     },
-    latitude: {
-        type: Number,
-        required: true,
-    },
-    longitude: {
-        type: Number,
-        required: true,
+    address: {
+        number: {
+            type: String,
+            required: false,
+        },
+        street: {
+            type: String,
+            required: false,
+        },
+        zipCode: {
+            type: String,
+            required: false,
+        },
+        city: {
+            type: String,
+            required: false,
+        },
+        coordinate: {
+            latitude: {
+                type: Number,
+                required: true,
+            },
+            longitude: {
+                type: Number,
+                required: true,
+            },
+        },
     },
     establishment_type: {
         type: Schema.Types.ObjectId,
-        ref: "establishmentTypes",
+        ref: "establishmenttypes",
     },
 });
-
 interface IEstablishment {
     name: string;
     establishment_type_id: string;
-    latitude: string;
-    longitude: string;
+    address: {
+        number: string;
+        street: string;
+        zipCode: string;
+        city: string;
+        coordinate: {
+            latitude: string;
+            longitude: number;
+        };
+    };
     establishment_type: ITypeEstablishment;
 }
-
 type EstablishmentType = IEstablishment & Document;
-
 const EstablishmentModel: Model<EstablishmentType> = model(
     "establishments",
     EstablishmentSchema
 );
-
 export { EstablishmentModel, IEstablishment, EstablishmentType };
