f5d2fd4a7796411e1f37a1e16b6066663b5768fb
Antoine GUENEAU <gueneau.antoine@gmail.com>
2020-08-27 14:03:54 +0200

    fix cote

--- nightfall/services/score.ts
+++ nightfall/services/score.ts
@@ -1,6 +1,7 @@
 import fetchRequest from "../models/FetchRequest";
 
 export const getScores : any = async (top : Number, period : 'month' | 'year') => {
-  const all_ranking = (await fetchRequest({path: `/users/ranking?period='${period}'`, method: "GET"})).ranking
+  const all_ranking = (await fetchRequest({path: `/users/ranking?period=${period}`, method: "GET"})).ranking
+  console.log(  all_ranking.splice(0, top) )
   return all_ranking.splice(0, top)
 };
